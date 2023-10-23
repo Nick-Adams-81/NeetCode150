55ef834e55dba9fddbc9d2ebf9925959f7ef6f3c
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-23 14:27:10 -0700

    two sum 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import Arrays.ContinsDuplicate;
+import Arrays.TwoSum;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         ContinsDuplicate duplicate = new ContinsDuplicate();
         int[] myNums = {1, 2, 3};
         System.out.println(duplicate.containsDuplicate(myNums));
+
+        TwoSum twoSum = new TwoSum();
+        int[] moreNums = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum.twoSum(moreNums, 17)));
     }
 }
