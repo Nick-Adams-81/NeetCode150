7fca8c79ea355213c255c915f3c5d4da0cfb7417
Nick Adams <nick.pappas.adams@gmail.com>
2023-11-02 13:37:08 -0700

    valid sudoku

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import TwoPointers.ValidPalindrome;
 import Arrays.ContainsDuplicate;
 import Arrays.GroupAnagrams;
 import Arrays.TopKFrequentElements;
+import Arrays.ValidSudoku;
 
 import java.util.Arrays;
 
@@ -53,5 +54,19 @@ public class Main {
         TrappingRainWater rainWater = new TrappingRainWater();
         int[] moreAqua = {4, 2, 0, 3, 2, 5};
         System.out.println(rainWater.trap(moreAqua));
+
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        ValidSudoku sudoku = new ValidSudoku();
+        System.out.println(sudoku.isValidSudoku(board));
     }
 }
